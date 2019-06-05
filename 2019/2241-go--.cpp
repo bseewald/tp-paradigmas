cf4f7a99d2d3cc4199e934395b2a829ed7ff248b
Bruna Seewald <bruna@rarolabs.com.br>
2019-06-05 16:19:45 -0300

    wip go--

--- 2019/2241-go--.cpp
+++ 2019/2241-go--.cpp
@@ -1,50 +1,83 @@
 #include <iostream>
 #include <cstring>
-#include <tuple>
 
 using namespace std;
 
-tuple<int, int> go(int mat){
-
-    tuple <int, int> dim;
-    
-    get<0>(dim) = 0;
-    get<1>(dim) = 0;
-    
-    return dim;
-}
-
 int main(){
 
-    int n, p, pw, pb, l, c;
-    tuple <int, int> dimensions;
+    int n, p, l, c;
+    int b_dim, w_dim;
 
     cin >> n >> p; // dimensao do tabuleiro e numero de pecas
 
     int board[n][n];
+
+    // Zera matriz: sem pecas
     memset(board, 0, sizeof(board[0][0]) * n * n);
 
-    pb = pw = p;
-
-    // Black stones
-    while (pb > 0)
-    {
-        cin >> l >> c;
-        board[l][c] = 0;
-        pb--;
+    // Cada jogador coloca suas pecas no tabuleiro
+    for(int player=1; player<3; player++){
+        for(int i=0; i<p; i++){
+            cin >> l >> c;
+            board[l-1][c-1] = player;
+        }
     }
 
-    // White stones
-    while (pw > 0)
-    {
-        cin >> l >> c;
-        board[l][c] = 1;
-        pw--;
+    // print board
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            cout << "[" << i << "][" << j << "]: " << board[i][j] << endl;
+        }
+    }    
+
+    // 0: regiao sem peca
+    // 1: regiao com peca preta
+    // 2: regiao com peca branca
+    // 3: regiao com ambas as pecas
+
+    // Start Go--
+    b_dim = p;
+    w_dim = p;
+        
+    for (int k = n; k > 1; k--){
+        for (int i = 0; i < k; i++){
+            for (int j = 0; j < k; j++){
+                if(board[i][j+1] == 3 && board[i+1][j+1] == 3 && board[i+1][j] == 3){
+                    //regiao mista
+                    board[i][j] = 3;
+                }
+                else{
+                    if(board[i][j] == 1){
+                        if(board[i][j+1] != 2 && board[i+1][j+1] != 2 && board[i+1][j] != 2){
+                            board[i][j] = 1; // regiao preta
+                            b_dim++;
+                        }
+                        else
+                            board[i][j] = 3; // regiao mista
+                    }
+                    else{
+                        if(board[i][j] == 2){
+                            if(board[i][j+1] != 1 && board[i+1][j+1] != 1 && board[i+1][j] != 1){
+                                board[i][j] = 1; //regiao branca
+                                w_dim++;
+                            }
+                        }
+                        else
+                            board[i][j] = 3; // regiao mista
+                    }
+                }
+            }
+        }
     }
 
-    dimensions = go(board);
+    // print board
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            cout << "[" << i << "][" << j << "]: " << board[i][j] << endl;
+        }
+    }    
 
-    // blacks whites
-    cout << get<0>(dimensions) << " " << get<1>(dimensions) << endl;
+    // Total de dimensoes de cada jogador
+    cout << b_dim << " " << w_dim << endl;
     return 0;
 }
